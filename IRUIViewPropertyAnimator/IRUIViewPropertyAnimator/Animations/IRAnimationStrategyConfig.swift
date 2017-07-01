c25967c9e6f6ddd446f5069719d50cd58d041478
Info Reqd <inforeqd51@gmail.com>
2017-07-01 15:55:14 +1000

    refactor to extract animation strategy config

---
+++ IRUIViewPropertyAnimator/IRUIViewPropertyAnimator/Animations/IRAnimationStrategyConfig.swift
@@ -0,0 +1,51 @@
+//
+//  IRAnimationStrategy.swift
+//  IRUIViewPropertyAnimator
+//
+//  Created by Shefali Mistry on 1/7/17.
+//  Copyright © 2017 Info Reqd. All rights reserved.
+//
+
+import UIKit
+
+enum IRAnimationStrategyType {
+    case SingleProperty
+    case CornerRadius
+    case CustomBezier
+    case SpringEffect
+    case MultipleBlocks
+    case Transform
+    case BackgroundColor
+    case ReversedAnimation
+    case BoundsChange
+    case GradientStartEndPoint
+    case GradientLocation
+    case GradientColors
+}
+
+protocol IRAnimationStrategy {
+    func animate(view:UIView)
+}
+
+struct IRAnimationStrategyFactory {
+    let model : [IRAnimationStrategyType : IRAnimationStrategy] =
+        [
+            IRAnimationStrategyType.SingleProperty : SinglePropertyStrategy(),
+            IRAnimationStrategyType.CornerRadius : CornerRadiusStrategy(),
+            IRAnimationStrategyType.CustomBezier : CustomBezierStrategy(),
+            IRAnimationStrategyType.SpringEffect : SpringEffectStrategy(),
+            IRAnimationStrategyType.MultipleBlocks : MultipleBlocksStrategy(),
+            IRAnimationStrategyType.Transform : TransformStrategy(),
+            IRAnimationStrategyType.BackgroundColor : BackgroundColorStrategy(),
+            IRAnimationStrategyType.ReversedAnimation : ReversedAnimationStrategy(),
+            IRAnimationStrategyType.BoundsChange : BoundsChangeStrategy(),
+            IRAnimationStrategyType.GradientStartEndPoint : GradientStartEndPointStrategy(),
+            IRAnimationStrategyType.GradientLocation : GradientLocationStrategy(),
+            IRAnimationStrategyType.GradientColors : GradientColorsStrategy()
+    ]
+    
+    func strategyFor(type: IRAnimationStrategyType) -> IRAnimationStrategy {
+        return model[type]!
+    }
+}
+
